dd9a2b25283c323bb7b9d6d97600d46e70eb7391
Renato-napoli <renato.napoli75@gmail.com>
2025-02-18 18:13:12 -0300

    hooks e estrutura de modulos

---
+++ fundamentos/src/app/components/componente-hooks/hook.component.ts
@@ -0,0 +1,63 @@
+import {
+  AfterContentChecked,
+  AfterContentInit,
+  AfterViewChecked,
+  AfterViewInit,
+  Component,
+  DoCheck,
+  Input,
+  OnChanges,
+  OnDestroy,
+  OnInit,
+} from '@angular/core';
+
+@Component({
+  selector: 'hooks-component',
+  templateUrl: './hook.component.html',
+  styleUrls: ['./hook.component.scss'],
+})
+export class hooksComponent
+  implements
+    OnChanges,
+    OnInit,
+    DoCheck,
+    AfterContentInit,
+    AfterContentChecked,
+    AfterViewInit,
+    AfterViewChecked,
+    OnDestroy
+{
+  @Input() dadosEntrada: string = '';
+
+  ngOnChanges() {
+    console.log('ngOnChanges', this.dadosEntrada);
+  }
+
+  ngOnInit() {
+    console.log('ngOnInit');
+  }
+
+  ngDoCheck() {
+    console.log('ngDoCheck');
+  }
+
+  ngAfterContentInit() {
+    console.log('ngAfterContentInit');
+  }
+
+  ngAfterContentChecked() {
+    console.log('ngAfterContentChecked');
+  }
+
+  ngAfterViewInit() {
+    console.log('ngAfterViewInit');
+  }
+
+  ngAfterViewChecked() {
+    console.log('ngAfterViewChecked');
+  }
+
+  ngOnDestroy() {
+    console.log('ngOnDestroy');
+  }
+}
---
+++ fundamentos/src/app/components/primeiro-componente/pirmeiro-compoente.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'primeiro-componente',
+  templateUrl: './primeiro-componente.html',
+  styleUrls: ['./primeiro-componente.scss'],
+})
+export class PrimeiroComponente {}

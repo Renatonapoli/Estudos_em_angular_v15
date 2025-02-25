545ab1c2c6bdd3a7390866f9ee956f47b86b41e7
Renato-napoli <renato.napoli75@gmail.com>
2025-02-25 18:15:05 -0300

    Praticando diretivas

--- projetos-fundamentos/src/app/app.module.ts
+++ projetos-fundamentos/src/app/app.module.ts
@@ -5,9 +5,15 @@ import { AppComponent } from './app.component';
 import { ListaItemComponent } from './components/lista-items/lista-item.component';
 import { FormsModule } from '@angular/forms';
 import { BotaoComponent } from './components/botao/botao.component';
+import { DiretivasComponent } from './components/ngFor_ngIf/diretivas.component';
 
 @NgModule({
-  declarations: [AppComponent, ListaItemComponent, BotaoComponent],
+  declarations: [
+    AppComponent,
+    ListaItemComponent,
+    BotaoComponent,
+    DiretivasComponent,
+  ],
   imports: [BrowserModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent],

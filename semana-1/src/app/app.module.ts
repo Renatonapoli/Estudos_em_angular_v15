a14e59127cb971cde6ac19555ddf6ed44b935ccc
Renato-napoli <renato.napoli75@gmail.com>
2025-02-27 19:28:18 -0300

    Treinando diretivas estruturais e de controle

--- semana-1/src/app/app.module.ts
+++ semana-1/src/app/app.module.ts
@@ -7,6 +7,7 @@ import { FormsModule } from '@angular/forms';
 import { BotaoComponent } from './components/botao/botao.component';
 import { DiretivasComponent } from './components/ngFor_ngIf/diretivas.component';
 import { FiltroComponent } from './components/filtrar/filtrar.component';
+import { CrudComponent } from './components/Crud/crud.component';
 
 @NgModule({
   declarations: [
@@ -15,6 +16,7 @@ import { FiltroComponent } from './components/filtrar/filtrar.component';
     BotaoComponent,
     DiretivasComponent,
     FiltroComponent,
+    CrudComponent,
   ],
   imports: [BrowserModule, FormsModule],
   providers: [],

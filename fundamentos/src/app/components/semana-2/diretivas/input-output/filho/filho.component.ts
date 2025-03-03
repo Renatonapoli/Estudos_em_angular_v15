ce6a6641dd7880d7c10b5a4e498db40876140cf1
Renato-napoli <renato.napoli75@gmail.com>
2025-03-03 18:57:10 -0300

    Filtrando e deletando a lista

--- fundamentos/src/app/components/semana-2/diretivas/input-output/filho/filho.component.ts
+++ fundamentos/src/app/components/semana-2/diretivas/input-output/filho/filho.component.ts
@@ -7,11 +7,24 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class FilhoComponent {
   @Input() valorTarefaFilho!: string;
+  @Input() valorFiltro!: string;
+
   @Input() arrayItem: string[] = [];
+  @Input() novoArrayItems: string[] = [];
 
   @Output() inserirNaLista = new EventEmitter<string>();
+  @Output() filtrando = new EventEmitter<string>();
+  @Output() deletarItem = new EventEmitter<string>();
 
   inserirItemLista() {
     this.inserirNaLista.emit(this.valorTarefaFilho);
+  }
+
+  filtrar() {
+    this.filtrando.emit(this.valorFiltro);
+  }
+
+  deletar(itemDeletado: string) {
+    this.deletarItem.emit(itemDeletado);
   }
 }
--- fundamentos/src/app/components/semana-2/diretivas/input-output/input-output.component.ts
+++ fundamentos/src/app/components/semana-2/diretivas/input-output/input-output.component.ts
@@ -7,13 +7,28 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
 })
 export class InputOutputComponent {
   valorTarefa!: string;
+  valorFiltrado!: string;
 
   arrayTarefas: string[] = ['Limpar casa', 'Lavar o carro'];
+  novoArrayTarefas: string[] = [...this.arrayTarefas];
 
   inserirTarefaNaLista(novaTarefa: string) {
     if (novaTarefa.trim()) {
       this.arrayTarefas.push(novaTarefa);
-      console.log(this.arrayTarefas);
+      this.novoArrayTarefas = [...this.arrayTarefas];
     }
+  }
+
+  filtrarListaItem(itemFiltrado: string) {
+    this.novoArrayTarefas = this.arrayTarefas.filter((item) =>
+      item.toLowerCase().includes(itemFiltrado.toLowerCase())
+    );
+  }
+
+  deletar(tarefaDeletado: string) {
+    this.arrayTarefas = this.arrayTarefas.filter(
+      (item) => item !== tarefaDeletado
+    );
+    this.novoArrayTarefas = [...this.arrayTarefas];
   }
 }
